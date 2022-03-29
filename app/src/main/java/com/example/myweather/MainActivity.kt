473f74a254d7b9cc51500c246220dfc153ca159a
inetamagone <99735096+inetamagone@users.noreply.github.com>
2022-03-29 13:56:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myweather/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.myweather
+
+import android.net.Uri
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import android.widget.TextView
+import org.json.JSONObject
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val API_Key = "91db09ff13832921fd93739ff0fcc890"
+    private val CITY = "Riga"
+    val BASE_URL =
+        "https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API_Key"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide() // Hide statusBar
+        setContentView(R.layout.activity_main)
+        gettingWeather().execute()
+    }
+
+    inner class gettingWeather() : AsyncTask<String, Void, String>() {
+
+        override fun doInBackground(vararg params: String?): String? {
+            var response: String?
+            try {
+                response = URL(BASE_URL).readText(
+                    Charsets.UTF_8
+                )
+            } catch (e: Exception) {
+                response = null
+            }
+            return response
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            try {
+                /* Extracting JSON from the API */
+                val jsonObj = JSONObject(result)
+                val main = jsonObj.getJSONObject("main")
+                val sys = jsonObj.getJSONObject("sys")
+                val wind = jsonObj.getJSONObject("wind")
+                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+                val visibility = jsonObj.getInt("visibility")
+
+                val updatedAt: Long = jsonObj.getLong("dt")
+                val updatedAtText =
+                    "Updated at: " + SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.ENGLISH).format(
+                        Date(updatedAt * 1000)
+                    )
+                val temp = main.getString("temp") + "°C"
+                val tempMin = "Min Temp: " + main.getString("temp_min") + "°C"
+                val tempMax = "Max Temp: " + main.getString("temp_max") + "°C"
+                val pressure = main.getString("pressure")
+                val humidity = main.getString("humidity")
+
+                val windSpeed = wind.getString("speed")
+                val weatherDescription = weather.getString("description")
+                val icon = weather.getString("icon")
+                val imageUri = Uri.parse("http://openweathermap.org/img/wn/$icon@2x.png")
+                val address = jsonObj.getString("name") + ", " + sys.getString("country")
+
+                /* Populating extracted data into the views */
+                findViewById<ImageView>(R.id.image_main).setImageURI(imageUri)
+                findViewById<TextView>(R.id.city_name).text = address
+                findViewById<TextView>(R.id.updated_time).text = updatedAtText
+                findViewById<TextView>(R.id.conditions).text = weatherDescription.capitalize()
+                findViewById<TextView>(R.id.temperature).text = temp
+                findViewById<TextView>(R.id.temp_min).text = tempMin
+                findViewById<TextView>(R.id.temp_max).text = tempMax
+
+                findViewById<TextView>(R.id.wind_data).text = "$windSpeed m/s"
+                findViewById<TextView>(R.id.humidity_data).text = "$humidity %"
+                findViewById<TextView>(R.id.pressure).text = "$pressure hPa"
+                findViewById<TextView>(R.id.visibility_data).text = "$visibility m"
+
+            } catch (e: Exception) {
+                println("Exception: $e")
+            }
+        }
+    }
+}
+
