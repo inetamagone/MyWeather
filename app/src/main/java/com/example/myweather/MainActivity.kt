e6bc6d3727e4ac8b8f47ace909b463c38fd81ab2
inetamagone <99735096+inetamagone@users.noreply.github.com>
2022-03-30 09:10:19 +0300

    Try to get image icon
    Try to get image icon

--- app/src/main/java/com/example/myweather/MainActivity.kt
+++ app/src/main/java/com/example/myweather/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.example.myweather
 
-import android.net.Uri
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.AsyncTask
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONObject
+import java.io.BufferedInputStream
+import java.io.IOException
 import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,6 +28,7 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide() // Hide statusBar
         setContentView(R.layout.activity_main)
         gettingWeather().execute()
+
     }
 
     inner class gettingWeather() : AsyncTask<String, Void, String>() {
@@ -41,6 +44,24 @@ class MainActivity : AppCompatActivity() {
             }
             return response
         }
+
+//        private fun getImageBitmap(url: String): Bitmap? {
+//            var bm: Bitmap? = null
+//            try {
+//                val aURL = URL(url)
+//                val conn = aURL.openConnection()
+//                conn.connect()
+//                val `is` = conn.getInputStream()
+//                val bis = BufferedInputStream(`is`)
+//                bm = BitmapFactory.decodeStream(bis)
+//                bis.close()
+//                `is`.close()
+//                println("image URL cached")
+//            } catch (e: IOException) {
+//                println("Error getting bitmap: $e")
+//            }
+//            return bm
+//        }
 
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
@@ -66,12 +87,15 @@ class MainActivity : AppCompatActivity() {
 
                 val windSpeed = wind.getString("speed")
                 val weatherDescription = weather.getString("description")
-                val icon = weather.getString("icon")
-                val imageUri = Uri.parse("http://openweathermap.org/img/wn/$icon@2x.png")
+//                val icon = weather.getString("icon")
+//                val imageUrl = "http://openweathermap.org/img/wn/$icon@2x.png"
                 val address = jsonObj.getString("name") + ", " + sys.getString("country")
 
                 /* Populating extracted data into the views */
-                findViewById<ImageView>(R.id.image_main).setImageURI(imageUri)
+                
+//                val image = findViewById<ImageView>(R.id.image_main)
+//                image.setImageBitmap(getImageBitmap(imageUrl))
+
                 findViewById<TextView>(R.id.city_name).text = address
                 findViewById<TextView>(R.id.updated_time).text = updatedAtText
                 findViewById<TextView>(R.id.conditions).text = weatherDescription.capitalize()
