bd85bf14c00076c8bce8a78cea291826443a12e9
inetamagone <99735096+inetamagone@users.noreply.github.com>
2022-04-01 15:52:43 +0300

    Search and getting weather reduced to one class
    Search and getting weather reduced to one class

--- app/src/main/java/com/example/myweather/FirstFragment.kt
+++ app/src/main/java/com/example/myweather/FirstFragment.kt
@@ -21,28 +21,29 @@ import java.util.*
 private const val TAG = "FirstFragment"
 
 private val API_Key = "91db09ff13832921fd93739ff0fcc890"
-private var CITY = "Riga"
-val BASE_URL =
-    "https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API_Key"
+private var CITY = ""
 
 class FirstFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        gettingWeather().execute()
+        Log.d(TAG, "onCreate called")
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Search function
         val view = inflater.inflate(R.layout.fragment_first, container, false)
+        // API call here
+        getWeather().execute()
+        // Search function
         val searchIcon = view.findViewById<ImageView>(R.id.search_icon)
         searchIcon.setOnClickListener {
-            searchCity().execute()
+            getWeather().execute()
             Log.d(TAG, "Search Button clicked")
         }
+        Log.d(TAG, "OnCreateView called")
         return view
     }
 
@@ -53,69 +54,28 @@ class FirstFragment : Fragment() {
         val navController = Navigation.findNavController(view)
 
         view.findViewById<Button>(R.id.button_next).setOnClickListener {
+            Log.d(TAG, "Button clicked to navigate to the SecondFragment")
             navController.navigate(R.id.action_firstFragment_to_secondFragment)
         }
     }
 
-    inner class gettingWeather() : AsyncTask<String, Void, String>() {
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        super.onSaveInstanceState(outState)
+//        Log.d(TAG, "Saved Instance State")
+//        val cityString = view?.findViewById<TextView>(R.id.city_name)?.text
+//        outState.putCharSequence("Saved city", cityString)
+//        Log.d(TAG, "Saved city $cityString")
+//    }
+//     /* On rotation onCreateView() calls twice, the first time CITY is stored but on the second time is null and displays null */
+//    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+//        super.onViewStateRestored(savedInstanceState)
+//        Log.d(TAG, "View State Restored")
+//        var storedCity = savedInstanceState?.getCharSequence("Saved city")
+//        CITY = storedCity.toString()
+//        Log.d(TAG, "Restored city $storedCity")
+//    }
 
-        override fun doInBackground(vararg params: String?): String? {
-            var response: String?
-            try {
-                response = URL(BASE_URL).readText(
-                    Charsets.UTF_8
-                )
-            } catch (e: Exception) {
-                response = null
-            }
-            return response
-        }
-
-        @SuppressLint("SetTextI18n")
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
-            try {
-                /* Extracting JSON from the API */
-                val jsonObj = JSONObject(result)
-                val main = jsonObj.getJSONObject("main")
-                val sys = jsonObj.getJSONObject("sys")
-                val wind = jsonObj.getJSONObject("wind")
-                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
-
-                val updatedAt: Long = jsonObj.getLong("dt")
-                val upDatedAtText =
-                    "Updated at: " + SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.ENGLISH).format(
-                        Date(updatedAt * 1000)
-                    )
-                val temp = main.getString("temp") + "°C"
-                val tempMin = "Min Temp: " + main.getString("temp_min") + "°C"
-                val tempMax = "Max Temp: " + main.getString("temp_max") + "°C"
-                val pressure = main.getString("pressure")
-                val humidity = main.getString("humidity")
-
-                val windSpeed = wind.getString("speed")
-                val weatherDescription = weather.getString("description")
-                val address = jsonObj.getString("name") + ", " + sys.getString("country")
-
-                /* Populating extracted data into the views */
-                view?.findViewById<TextView>(R.id.city_name)?.text = address
-                view?.findViewById<TextView>(R.id.updated_time)?.text = upDatedAtText
-                view?.findViewById<TextView>(R.id.conditions)?.text = weatherDescription
-                view?.findViewById<TextView>(R.id.temperature)?.text = temp
-                view?.findViewById<TextView>(R.id.temp_min)?.text = tempMin
-                view?.findViewById<TextView>(R.id.temp_max)?.text = tempMax
-                view?.findViewById<TextView>(R.id.pressure)?.text = pressure
-                view?.findViewById<TextView>(R.id.wind_data)?.text = "$windSpeed m/s"
-                view?.findViewById<TextView>(R.id.humidity_data)?.text = "$humidity %"
-                view?.findViewById<TextView>(R.id.pressure)?.text = "$pressure hPa"
-                Log.d(TAG, "onPostExecute Called")
-            } catch (e: Exception) {
-                Log.d(TAG, "Exception: $e")
-            }
-        }
-    }
-
-    inner class searchCity() : AsyncTask<String, Void, String>() {
+    inner class getWeather() : AsyncTask<String, Void, String>() {
 
         private var CITY = getCity()
         val BASE_URL =
@@ -181,9 +141,13 @@ class FirstFragment : Fragment() {
 
     fun getCity(): String {
         val editCity = view?.findViewById<TextInputEditText>(R.id.edit_city)
-        CITY = editCity?.text.toString()
-        Log.v(TAG, "Change cityName: $CITY")
-        editCity?.setText("")
+        if (editCity?.text.isNullOrEmpty()) {
+            CITY = "Riga"
+        } else {
+            CITY = editCity?.text.toString()
+            Log.v(TAG, "Change cityName: $CITY")
+            editCity?.setText("")
+        }
         return CITY
     }
 }
